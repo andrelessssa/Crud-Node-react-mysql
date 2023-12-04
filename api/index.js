7ca2e9f46c011dbc0e31647cfdd0e7fb617fb44c
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-12-04 17:12:15 -0300

    teste
    teste

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import cors from "cors";
+const express = require("express")
+const cors = require("cors");
 import userRoutes from "./routes/users.js"
 
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/", userRoutes)
+
 
 app.listen(8800);
 
